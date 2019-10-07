aa3c221054acba92b04c346664941f11db104560
kazeshan <kazeshan@gmail.com>
2019-10-07 11:04:45 +0800

    1 file modified, 1 deleted

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./style.css";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      name: 'React'
-    };
   }
 
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
+        <h2>Contact List Application</h2>
+        <h3>Step 1</h3>
         <p>
-          Start editing to see some magic happen :)
+          Mimic the functionality of a phones contact list. Fetch list of
+          contacts from this URL => https://jsonplaceholder.typicode.com/users
         </p>
+        All contacts should be displayed in a list on the Contact page (username
+        only)
+        <br />
+        <br />
+        Additional details for a specific contact should either be displayed on
+        a new page or in a modal when the contact is selected
+        <br />
+        <br />A contact should consist of :
+        <ul>
+          <li>name,</li>
+          <li>email,</li>
+          <li>phone,</li>
+          <li>company</li>
+          <li>photo</li>
+        </ul>
+        <h3>Step 2</h3>
+        <ul>
+          <li>Display the contact list alphabetically</li>
+          <li>Allow the user to take a photo using the camera</li>
+          <li>
+            Use local storage to persist contacts after the application is
+            reloaded
+          </li>
+        </ul>
+        <h3>Step 3</h3>
+        <ul>
+          <li>A user should be able to add new contacts to the application</li>
+          <li>A specific contact should be able to be edited & deleted</li>
+        </ul>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
