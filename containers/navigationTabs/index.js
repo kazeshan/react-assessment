1daf911911086670c0d4597dd80e8a97d237ee70
kazeshan <kazeshan@gmail.com>
2019-10-07 14:30:07 +0800

    1 file modified

--- containers/navigationTabs/index.js
+++ containers/navigationTabs/index.js
@@ -13,19 +13,19 @@ export default class NavigationTabs extends Component {
       <div className="nav_master">
         <NavigationTab
           text={"Home"}
-          icon={<i class="material-icons">home</i>}
+          icon={<i className="material-icons">home</i>}
           onclick={this.props.switchTab.bind(null, "HOME")}
           selected={this.props.currentTab === "HOME"}
         />
         <NavigationTab
           text={"About"}
-          icon={<i class="material-icons">info</i>}
+          icon={<i className="material-icons">info</i>}
           onclick={this.props.switchTab.bind(null, "ABOUT")}
           selected={this.props.currentTab === "ABOUT"}
         />
         <NavigationTab
           text={"Contatct"}
-          icon={<i class="material-icons">contacts</i>}
+          icon={<i className="material-icons">contacts</i>}
           onclick={this.props.switchTab.bind(null, "CONTACT")}
           selected={this.props.currentTab === "CONTACT"}
         />
